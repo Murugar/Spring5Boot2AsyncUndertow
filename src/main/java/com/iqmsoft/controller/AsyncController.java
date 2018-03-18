2fa083e9b7e39ed7fbd9020a7e22a257b37a9bc1
Davanon <davanon2014@gmail.com>
2018-03-18 11:28:00 +0530

    First

--- src/main/java/com/iqmsoft/controller/AsyncController.java
+++ src/main/java/com/iqmsoft/controller/AsyncController.java
@@ -2,9 +2,6 @@ package com.iqmsoft.controller;
 
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 @Log
 @RestController
@@ -30,25 +26,5 @@ public class AsyncController {
 
         log.info("End get.");
         return emitter;
-    }
-
-    @Log
-    @Component
-    public static class AsyncHelper {
-       
-        @Async
-        public void streaming(ResponseBodyEmitter emitter, long eventNumber, long intervalSec) throws IOException, InterruptedException {
-            log.info("Start Async processing.");
-
-            for (long i = 1; i <= eventNumber; i++) {
-                TimeUnit.SECONDS.sleep(intervalSec);
-                emitter.send("message coming " + i + "\r\n", MediaType.TEXT_PLAIN);
-            }
-
-            emitter.complete();
-
-            log.info("End Async processing.");
-        }
-       
     }
 }
